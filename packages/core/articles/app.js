a5c9ba06c4fa6d07b255d0299194c5ee172d9ccf
Grant Campanelli <grantcampanelli@gmail.com>
2016-01-12 11:16:17 -0800

    Returned event to article

--- packages/core/articles/app.js
+++ packages/core/articles/app.js
@@ -22,12 +22,12 @@ Articles.register(function(app, auth, database, circles, swagger) {
   //We are adding a link to the main menu for all authenticated users
   Articles.menus.add({
     'roles': ['authenticated'],
-    'title': 'Events',
+    'title': 'Articles',
     'link': 'all articles'
   });
   Articles.menus.add({
     'roles': ['authenticated'],
-    'title': 'Create New Event',
+    'title': 'Create New Article',
     'link': 'create article'
   });
 

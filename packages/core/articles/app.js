70cb0038ede66fbe0843683001958ecacbdc18f5
Grant Campanelli <grantcampanelli@gmail.com>
2016-01-12 02:16:42 -0800

    Change two things to Event

--- packages/core/articles/app.js
+++ packages/core/articles/app.js
@@ -22,12 +22,12 @@ Articles.register(function(app, auth, database, circles, swagger) {
   //We are adding a link to the main menu for all authenticated users
   Articles.menus.add({
     'roles': ['authenticated'],
-    'title': 'Articles',
+    'title': 'Events',
     'link': 'all articles'
   });
   Articles.menus.add({
     'roles': ['authenticated'],
-    'title': 'Create New Article',
+    'title': 'Create New Event',
     'link': 'create article'
   });
 
